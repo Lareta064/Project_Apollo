314b092d89ed578cfc78373dc6c57863c6579cca
Lareta <lareta1@yandex.ru>
2017-11-22 19:35:52 +0300

    apollo-3

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
 	// Скрипт для показа и скрытия выпадающего меню на смартфонах
 	// Создаем переменые для кнопки и для меню
 	var pull = $('#navigation-toggle');
@@ -19,18 +21,37 @@ $(document).ready(function() {
 	    
 	});
 
-	// При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
+
+
+	// При изменении размера окна, в большую сторону, если меню было скрыто, показываем его
+	// И у кнопки убираем модификатор --active
 	$(window).resize(function(){
 	    var w = $(window).width();
 	    if(w > 992) {
 	        menu.removeAttr('style');
-	    };
-	    if(w < 992) {
-	        $('nav.navigation a').on("click", function(){
-	        menu.slideToggle();	
-	        })
+	        pull.removeClass('navigation__toggle-button--active');
+	    } else {
+
 	    }
 	});
+
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('nav.navigation a').on("click", function(){
+		fnstart();
+	});
+
+	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( $("#navigation-toggle").hasClass("navigation__toggle-button--active")  ) {
+   			pull.toggleClass('navigation__toggle-button--active');
+			menu.slideToggle();
+		}
+	};
+
+
 
 
 	// Вызов слайдера owl-carousel
